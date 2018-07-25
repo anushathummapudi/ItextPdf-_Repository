0542f3fefee7626efc9015568b1862968f7e0238
delegate <anushathummapudi95@gmail.com>
2018-07-25 13:03:18 +0530

    first commit

---
+++ IText/src/com/virtusa/pdf/PdfGenerator.java
@@ -0,0 +1,36 @@
+package com.virtusa.pdf;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import com.itextpdf.text.Anchor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.CMYKColor;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class PdfGenerator {
+public static void main(String args[]) throws DocumentException, FileNotFoundException
+{
+	Document document = new Document(PageSize.A4, 50, 50, 50, 50);
+	PdfWriter writer = PdfWriter.getInstance(document,  new FileOutputStream("sample.pdf"));
+			 
+			document.open();
+			Anchor anchorTarget = new Anchor("First page of the document.");
+		      anchorTarget.setName("BackToTop");
+		      Paragraph paragraph1 = new Paragraph();
+		 
+		      paragraph1.setSpacingBefore(50);
+		 
+		      paragraph1.add(anchorTarget);
+		      document.add(paragraph1);
+		 
+		document.add(new Paragraph("Some more text on the \first page with different color and font type.", FontFactory.getFont(FontFactory.COURIER, 14, Font.BOLD, new CMYKColor(0, 255, 0, 0))));
+		document.close();
+	System.out.println("hello");
+}
+}
